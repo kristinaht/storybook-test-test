94212c3b06ba69d414ee99c9bf22bea8adc41383
Kristina Hengster Tintor <hengster.tintor@gmail.com>
2020-09-03 19:28:26 -0700

    add redux with store & initial state

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -2,8 +2,10 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import Task from './Task';
+import { connect } from 'react-redux';
+import { archiveTask, pinTask} from '../lib/redux';
 
-export default function TaskList({ loading, tasks, onPinTask, onArchiveTastk}) {
+export function PureTaskList({ loading, tasks, onPinTask, onArchiveTastk}) {
   const events = {
     onPinTask,
     onArchiveTastk
@@ -54,7 +56,7 @@ export default function TaskList({ loading, tasks, onPinTask, onArchiveTastk}) {
   )
 }
 
-TaskList.propTypes = {
+PureTaskList.propTypes = {
   //checks if it's in loading state
   loading: PropTypes.bool,
   //the list of tasks:
@@ -62,8 +64,19 @@ TaskList.propTypes = {
   //event to change the task to pinned:
   onPinTask: PropTypes.func,
   //envent to change the task to archived:
-  onArchiveTastk: PropTypes.func
+  onArchiveTask: PropTypes.func,
 };
-TaskList.defaultProps = {
+
+PureTaskList.defaultProps = {
   loading: false,
 }
+
+export default connect(
+  ({ tasks }) => ({
+    tasks: tasks.filter(t => t.state === 'TASK_INBOX' || t.state === 'TASK_PINNED'),
+  }),
+  dispatch => ({
+    onArchiveTask: id => dispatch(archiveTask(id)),
+    onPinTask: id => dispatch(pinTask(id)),
+  })
+)(PureTaskList);
